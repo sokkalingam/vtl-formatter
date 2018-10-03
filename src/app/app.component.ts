ef2a6455b3ea6dd6cfcbd8b1a559ea1d015f78d4
Sokkalingam Subramanian <sokkalingam.subramanian@zoominfo.com>
2018-10-03 16:06:47 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'vtl-formatter';
+  title = "VTL Formatter";
+  text = "";
 }
